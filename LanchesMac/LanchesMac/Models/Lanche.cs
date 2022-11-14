9ba3774343f74742d0ce77d4ffe00cda3bef14ce
tarcisiokta2012@gmail.com <tarcisiokta2012@gmail.com>
2022-11-13 22:40:17 -0300

    Installing Entity FrameWork

---
+++ LanchesMac/LanchesMac/Models/Lanche.cs
@@ -0,0 +1,17 @@
+﻿namespace LanchesMac.Models
+{
+    public class Lanche
+    {
+        public int LancheId { get; set; }
+        public string Nome { get; set; }
+        public string DescricaoCurta { get; set; }
+        public string DescricaoDetalhada { get; set; }
+        public decimal Preco { get; set; }
+        public string ImagemUrl { get; set; }
+        public string ImagemThumbnail { get; set; }
+        public bool IsLanchePreferido { get; set; }
+        public bool EmEstoque { get; set; }
+        public int CategoriaId { get; set; }
+        public virtual Categoria Categoria { get; set; }
+    }
+}
--- LanchesMac/LanchesMac/Program.cs
+++ LanchesMac/LanchesMac/Program.cs
@@ -1,3 +1,6 @@
+using LanchesMac.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -12,6 +15,9 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
