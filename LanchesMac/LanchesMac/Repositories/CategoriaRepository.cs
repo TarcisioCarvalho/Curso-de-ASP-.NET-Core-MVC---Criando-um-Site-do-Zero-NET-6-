6389ee76085609eb1904c0611c25ebea4720e049
tarcisiokta2012@gmail.com <tarcisiokta2012@gmail.com>
2022-11-15 15:59:09 -0300

    Interface Categorias

---
+++ LanchesMac/LanchesMac/Repositories/CategoriaRepository.cs
@@ -0,0 +1,18 @@
+﻿using LanchesMac.Data;
+using LanchesMac.Models;
+using LanchesMac.Repositories.Interfaces;
+
+namespace LanchesMac.Repositories
+{
+    public class CategoriaRepository:ICategoriaRepository
+    {
+        private readonly AppDbContext _appDbContext;
+
+        public CategoriaRepository(AppDbContext appDbContext)
+        {
+            _appDbContext = appDbContext;
+        }
+
+        public IEnumerable<Categoria> Categorias => _appDbContext.Categorias;
+    }
+}
