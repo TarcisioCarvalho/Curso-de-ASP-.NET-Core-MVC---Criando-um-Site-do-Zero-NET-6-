b90fb33caa6f908a9345a327dd73d8b9c5589fb2
tarcisiokta2012@gmail.com <tarcisiokta2012@gmail.com>
2022-11-15 15:35:46 -0300

    Popular Categorias Migration

--- LanchesMac/LanchesMac/Program.cs
+++ LanchesMac/LanchesMac/Program.cs
@@ -3,6 +3,9 @@ using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
@@ -15,9 +18,6 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
-
-builder.Services.AddDbContext<AppDbContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
