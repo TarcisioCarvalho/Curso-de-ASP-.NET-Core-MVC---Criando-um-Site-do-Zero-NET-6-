5af901fa370351ac0466bb7af419d031454fd8b3
tarcisiokta2012@gmail.com <tarcisiokta2012@gmail.com>
2022-11-16 21:44:20 -0300

    Injecao de dependencia

--- LanchesMac/LanchesMac/Program.cs
+++ LanchesMac/LanchesMac/Program.cs
@@ -1,10 +1,15 @@
 using LanchesMac.Data;
+using LanchesMac.Repositories;
+using LanchesMac.Repositories.Interfaces;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddTransient<ICategoriaRepository,CategoriaRepository>();
+builder.Services.AddTransient<ILancheRepository, LancheRepository>();
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
---
+++ LanchesMac/LanchesMac/Repositories/LancheRepository.cs
@@ -0,0 +1,27 @@
+﻿using LanchesMac.Data;
+using LanchesMac.Models;
+using LanchesMac.Repositories.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace LanchesMac.Repositories
+{
+    public class LancheRepository : ILancheRepository
+    {
+        private readonly AppDbContext _context;
+
+        public LancheRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public IEnumerable<Lanche> Lanches => _context.Lanches.Include(c => c.Categoria);
+
+        public IEnumerable<Lanche> LanchesPreferidos => _context.Lanches.Where(i => i.IsLanchePreferido)
+            .Include(c => c.Categoria);
+
+        public Lanche GetLancheById(int lancheId)
+        {
+            return _context.Lanches.FirstOrDefault(i => i.LancheId == lancheId);
+        }
+    }
+}
