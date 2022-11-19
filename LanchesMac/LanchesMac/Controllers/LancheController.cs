ab0c60eea652d860da6e5bb1cf856828e61510a8
tarcisiokta2012@gmail.com <tarcisiokta2012@gmail.com>
2022-11-19 14:23:28 -0300

    Create Lanche Controller and List View

---
+++ LanchesMac/LanchesMac/Controllers/LancheController.cs
@@ -0,0 +1,22 @@
+﻿using LanchesMac.Repositories;
+using LanchesMac.Repositories.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LanchesMac.Controllers
+{
+    public class LancheController : Controller
+    {
+        private readonly ILancheRepository _lancheRepository;
+
+        public LancheController(ILancheRepository lancheRepository)
+        {
+            _lancheRepository = lancheRepository;
+        }
+
+        public IActionResult List()
+        {
+            var lanches = _lancheRepository.Lanches;
+            return View(_lancheRepository.Lanches);
+        }
+    }
+}
